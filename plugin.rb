5e0afd67fe6130eb812f8fb96810aca2d47a273c
Robert Barrow <merefield@gmail.com>
2022-04-27 08:59:25 +0100

    clean up code

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: x-discourse-solved-qna-interop
 # about: Extend Discourse Solved & Question n Answer plugins and make them work together as specced.
-# version: 0.0.4
+# version: 0.0.5
 # url: https://github.com/paviliondev/x-discourse-solved-qna-interop
 # authors: merefield
 
@@ -178,30 +178,6 @@ after_initialize do
     end
   end
 
-  # module QuestionAnswer
-  #   module AdditionalPostSerializerExtension
-  #     # def self.included(base)
-  #     #   base.attributes(
-  #     #     :qa_vote_count,
-  #     #     :solution,
-  #     #     :solution_actor_user_id,
-  #     #     :qa_user_voted_direction,
-  #     #     :qa_has_votes,
-  #     #     :comments,
-  #     #     :comments_count,
-  #     #   )
-  #     # end
-  
-  #     def solution
-  #      object.solution
-  #     end
-
-  #     def solution_actor_user_id
-  #       object.solution_actor_user_id
-  #     end
-  #   end
-  # end
-
   TopicView.apply_custom_default_scope do |scope, topic_view|
     if topic_view.topic.is_qa? &&
       !topic_view.instance_variable_get(:@replies_to_post_number) &&
@@ -220,10 +196,6 @@ after_initialize do
       scope
     end
   end
-
-  # class ::PostSerializer
-  #   include QuestionAnswer::AdditionalPostSerializerExtension
-  # end
 
   add_to_serializer(:post,:solution) {object.solution}
   add_to_serializer(:post,:solution_actor_user_id) {object.solution_actor_user_id}
